d4ded0cbd04c95d063f7742d615b9078e3a7e2aa
demo1 <limbanijeet8@gmail.com>
2025-06-20 13:52:01 +0530

    [FEAT]:form-types+routes(admin+user)

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AdminDashboard from '../pages/admin/AdminDashboard';
+import UserDashboard from '../pages/user/UserDashboard';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/admin" element={<AdminDashboard />}></Route>
+      <Route path="/user" element={<UserDashboard />}></Route>
+      <Route></Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
