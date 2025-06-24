21a829b9fa84d4415c5d959039a84b7b4e2e3b3a
Harsh Patel <harshp.patel@bacancy.com>
2025-06-24 10:43:21 +0530

    [FEAT] : ecf-s2-5-Reusable-Table (#25)

---
+++ src/constants/ColumnTypes.js
@@ -0,0 +1,16 @@
+export const ColumnTypes = {
+  category: [
+    { field: 'name', headerName: 'Category Name' },
+    { field: 'image', headerName: 'Image' },
+    { field: 'actions', headerName: 'Actions' },
+  ],
+
+  product: [
+    { field: 'name', headerName: 'Product Name' },
+    { field: 'price', headerName: 'Price' },
+    { field: 'stock', headerName: 'Stock' },
+    { field: 'category', headerName: 'Category' },
+    { field: 'images', headerName: 'Image' },
+    { field: 'actions', headerName: 'Actions' },
+  ],
+};
