5dd1fe97732df252ff6f5937fc37b2f34ab503ed
Princy-Patel-30 <princy.patel@bacancy.com>
2025-06-24 11:30:21 +0530

    Ecf 3 reusable form component (#26)
    * FEAT : [reusable form + card ]
    
    * feat : [global toastprovider added]
    
    * [feat]: added protected route
    
    * [RESOLVE]: merge conflicts + user/admin protected routes
    
    * [REFACTOR]:added dynamic roles to the protected routes
    
    * [REFACTOR]:added dynamic roles to the protected routes
    
    ---------
    
    Co-authored-by: Yash-Varmora <yash.varmora@bacancy.com>

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import AdminDashboard from '../pages/admin/AdminDashboard';
 import Login from '../pages/LoginPage';
 import Register from '../pages/RegisterPage';
-import UserDashboard from '../pages/user/UserDashboard';
-// import ProtectedRoute from './ProtectedRoute';
-import ForgotPassword from '../pages/ForgotPassword';
-import ChangePassword from '../pages/user/ChangePaasword';
 import ResetPassword from '../pages/user/ResetPassword';
+import ChangePassword from '../pages/user/ChangePaasword';
 import LandingPage from '../pages/LandingPage';
-
+import UserDashboard from '../pages/user/UserDashboard';
+import ForgotPassword from '../pages/ForgotPassword';
+import AdminProtectedRoute from './AdminProtectedRoute';
+import UserProtectedRoute from './UserProtectedRoute';
 const AppRoutes = () => {
   return (
     <Routes>
@@ -16,15 +16,15 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/admin/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/user" element={<UserDashboard />} />
       <Route path="/reset-password/:token" element={<ResetPassword />} />
       <Route path="/change-password" element={<ChangePassword />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      {/* <Route element={<ProtectedRoute />}> */}
-      <Route path="/admin" element={<AdminDashboard />} />
-
-      {/* </Route> */}
+      <Route element={<AdminProtectedRoute />}>
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Route>
+      <Route element={<UserProtectedRoute />}>
+        <Route path="/user" element={<UserDashboard />} />
+      </Route>
     </Routes>
   );
 };
