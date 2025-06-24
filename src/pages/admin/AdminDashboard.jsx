6fa381bcecfeb7e6b14b6e3d4323487a200f8c51
Dhyan-dp <dhyan.patel@bacancy.com>
2025-06-24 15:05:16 +0530

    Ecf s2 1 navbar product card (#29)
    * [commit before pull]
    
    * [pull before merge]
    
    * [FEAt]:resuable user and login navbar is added
    
    ---------
    
    Co-authored-by: Dhyanp1771 <dhyanp1771@gmail.com>

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
-import { logoutUser } from '../../services/AuthService';
-import { logout } from '../../redux/slices/auth';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 const AdminDashboard = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  return (
-    <div>
-      admin
-      <button
-        onClick={async () => {
-          await logoutUser();
-          dispatch(logout());
-          navigate('/login');
-        }}
-      >
-        Logout
-      </button>
-    </div>
-  );
+  return <div>admin</div>;
 };
 
 export default AdminDashboard;
