3fdd035c0fa0ecb8723b2a2be57990d5b4c5a2c8
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-27 13:23:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
 import ToastProvider from './components/common/ToastProvider';
 import AppRoutes from './routes/AppRoutes';
-import { useEffect, useRef } from 'react';
+import { useEffect,useRef } from 'react';
 import Cookies from 'js-cookie';
 import { fetchMe } from './redux/slices/auth';
-// import Loading from './components/common/Loading';
 import { useLocation } from 'react-router-dom';
 import LoadingSpinner from './components/common/LoadingSpinner';
 
 const App = () => {
   const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.auth);
   const location = useLocation();
-
-  const isAuthRoute = ['/login', '/register', '/forgot-password', '/reset-password'].some((path) =>
-    location.pathname.startsWith(path),
-  );
+  const { loading } = useSelector((state) => state.auth);
 
   const token = Cookies.get('accessToken');
   const hasFetched = useRef(false);
+
+  const isAuthRoute = ['/login', '/register', '/forgot-password', '/reset-password'].some((path) =>
+    location.pathname.startsWith(path)
+  );
 
   useEffect(() => {
     if (token && !hasFetched.current) {
@@ -34,7 +33,7 @@ const App = () => {
   return (
     <>
       <AppRoutes />
-      <ToastProvider position="top-right" />
+      <ToastProvider position="top-right" autoClose={3000} hideProgressBar={false} />
     </>
   );
 };
