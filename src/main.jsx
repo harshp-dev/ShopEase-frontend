4775aa7a5b2e39d8699a8bd54262d6d95592bc1b
swanubhuti <swanubhuti.jain@bacancy.com>
2025-06-27 11:33:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,11 +9,11 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+   <StrictMode>
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  // </StrictMode>,
+   </StrictMode>,
 );
